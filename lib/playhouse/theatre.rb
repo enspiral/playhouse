1194347bb85b52545252c76a825ef4fbec440612
Craig Ambrose <craig@craigambrose.com>
2013-05-16 21:43:33 +1200

    Introduced theatre, doesn't entirely do what I want yet

---
+++ lib/playhouse/theatre.rb
@@ -0,0 +1,34 @@
+module Playhouse
+  class Theatre
+    attr_reader :root_path
+
+    def initialize(options)
+      @root_path = options[:root] || Dir.pwd
+      @environment = options[:environment] || (raise ArgumentError.new("environment option is required"))
+    end
+
+    def stage
+      start_staging
+      yield
+      stop_staging
+    end
+
+    def start_staging
+      Dir.chdir @root_path
+      connect_to_database
+    end
+
+    def stop_staging
+      nil
+    end
+
+    private
+
+    def connect_to_database
+      db_params = YAML.load(File.read(root_path + "/config/database.yml"))[@environment]
+      puts "CONNECT TO DB"
+      puts db_params.inspect
+      ActiveRecord::Base.establish_connection db_params
+    end
+  end
+end
